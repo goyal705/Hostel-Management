b57c441fd3abbaa53ed59cea6d3a86e762c528e1
goyal705 <75295353+goyal705@users.noreply.github.com>
2024-07-26 00:30:34 +0530

    Delete my-react-app/build directory

--- my-react-app/build/config.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["HostelManagementSystem"]
-studentsdata= db["StudentsData"]
-complaintsdata=db['Complaints']
-users=db['Users']
-roomsdata = db["RoomsData"]
